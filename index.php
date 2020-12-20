34acdbf94b42d3a5a0440c29a187cda502c76964
prang0108 <76036863+prang0108@users.noreply.github.com>
2020-12-20 16:32:24 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="th">
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>เว็บแนะนำตัว</title>
+  </head>
+  <style>
+body {
+  background-image: url('https://get.wallhere.com/photo/landscape-sea-water-shore-sand-sky-photography-beach-calm-coast-Canon-wind-horizon-coastline-low-Atlantic-cloud-travel-ocean-wave-seaside-minimal-digital-islands-tide-adventure-eos-7d-journey-fuerteventura-canarias-canary-wind-wave-coastal-and-oceanic-landforms-761979.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
+</style>
+        <h1>ประวัติส่วนตัว</h1>
+<p>ชื่อ-นามสกุล : จินตนา ณ วิเชียร</p>
+<p>เพศ : หญิง    อายุ : 20 ปี</p>
+<p>กำลังศึกษา : สาขาวิทยาการข้อมูล คณะวิทยาศาสตร์ มหาวิทยาลัยศิลปากร</p>
+<p>ข้อมูลการติดต่อ : </p>
+<a href="https://www.facebook.com/jintana.nawichian">Facebook : Jintana Na wichian
+<br>
+<br>
+<img src="https://www.flaticon.com/svg/static/icons/svg/124/124010.svg" width="100" height="100"</a>
+  </body>
+</html>
